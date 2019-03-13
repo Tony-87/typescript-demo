9e240def90dfbc8db791f12e63b8d118283b63c6
360BUYAD\wangxiaobo9 <wangxiaobo9@jd.com>
2019-03-13 12:59:07 +0800

    org demo

---
+++ org/greeter.js
@@ -0,0 +1,23 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color.Blue;
+alert(colorName); // 显示'Green'因为上面代码里它的值是2
---
+++ org/greeter.ts
@@ -0,0 +1,28 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
+
+
+
+// enum
+
+enum Color {Red = 1, Green, Blue}
+let colorName: string = Color[2];
+
+alert(colorName);  // 显示'Green'因为上面代码里它的值是2
